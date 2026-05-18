1a4548c66229608f8bf39c73a6dce4d0649688a6
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-05-18 17:59:18 +0000

    Add utility function to sort character counts by frequency in descending order

--- stats.py
+++ stats.py
@@ -10,3 +10,9 @@ def count_chars(text: str) -> dict:
             char_count[char] = char_count.get(char, 0) + 1
 
     return char_count
+
+
+def sort_by_value(chars: dict[str, int]) -> dict:
+    return dict(
+        sorted(chars.items(), key=lambda item: item[1], reverse=True)
+    )
