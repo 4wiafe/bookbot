327c10c495947a4dca56d87dfd95704ca61b3c99
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-05-18 23:59:30 +0000

    Add formatted report output function for book analysis results

---
+++ report.py
@@ -0,0 +1,8 @@
+def print_report(counted_words: int, sorted_chars: dict[str, int]) -> None:
+    print("============ BOOKBOT ============")
+    print("----------- Word Count ----------")
+    print(f"Found {counted_words:,} total words")
+    print("--------- Character Count -------")
+
+    for key, value in sorted_chars.items():
+        print(f"{key}: {value:,}")
